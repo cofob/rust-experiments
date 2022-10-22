31eef5a63f92b931d7f793a111b35397748da9de
Egor Ternovoy <cofob@riseup.net>
2022-10-22 18:54:46 +0700

    GCD webserver

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use std::io::Write;
+#[macro_use]
+extern crate mime;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
 use std::str::FromStr;
+use urlencoded::UrlEncodedBody;
 
 /// Get greatest common divisor of two numbers
 ///
@@ -36,29 +42,98 @@ fn test_gcd() {
 
 /// Main function
 fn main() {
-    // Check if arguments are given
-    if std::env::args().len() == 1 {
-        writeln!(
-            std::io::stderr(),
-            "Usage: {} NUMBERS...",
-            std::env::args().nth(0).unwrap()
-        )
-        .unwrap();
-        std::process::exit(1);
+    let mut router = Router::new();
+
+    router.get("/", get_form, "index");
+    router.post("/gcd", post_gcd, "post_gcd");
+
+    println!("Server running on port 3000: http://localhost:3000/");
+    Iron::new(router).http("localhost:3000").unwrap();
+}
+
+fn get_form(_request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(
+        r#"
+        <title>GCD Calculator</title>
+        <form action="/gcd" method="post">
+            <input type="text" name="n"/>
+            <input type="text" name="m"/>
+            <button type="submit">Compute GCD</button>
+        </form>
+    "#,
+    );
+    Ok(response)
+}
+
+fn post_gcd(request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    let form_data = match request.get_ref::<UrlEncodedBody>() {
+        Ok(map) => map,
+        Err(e) => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("Error parsing form data: {:?}\n", e));
+            return Ok(response);
+        }
+    };
+    let n = match form_data.get("n") {
+        Some(nums) => nums,
+        None => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("form data has no 'n' parameter\n"));
+            return Ok(response);
+        }
+    };
+    let m = match form_data.get("m") {
+        Some(nums) => nums,
+        None => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("form data has no 'm' parameter\n"));
+            return Ok(response);
+        }
+    };
+    let mut numbers = Vec::new();
+    for unparsed in n {
+        match u64::from_str(unparsed) {
+            Ok(n) => numbers.push(n),
+            Err(_) => {
+                response.set_mut(status::BadRequest);
+                response.set_mut(format!(
+                    "Value for 'n' parameter not a number: {:?}\n",
+                    unparsed
+                ));
+                return Ok(response);
+            }
+        }
+    }
+    for unparsed in m {
+        match u64::from_str(unparsed) {
+            Ok(m) => numbers.push(m),
+            Err(_) => {
+                response.set_mut(status::BadRequest);
+                response.set_mut(format!(
+                    "Value for 'n' parameter not a number: {:?}\n",
+                    unparsed
+                ));
+                return Ok(response);
+            }
+        }
     }
 
-    // Convert arguments to numbers
-    let numbers: Vec<u64> = std::env::args()
-        .skip(1)
-        .map(|arg| u64::from_str(&arg).expect("error parsing argument"))
-        .collect();
-
-    // Calculate gcd
     let mut d = numbers[0];
     for m in &numbers[1..] {
         d = gcd(d, *m);
     }
 
-    // Print result
-    println!("The greatest common divisor of {:?} is {}", numbers, d);
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(format!(
+        "The greatest common divisor of the numbers {:?} is <b>{}</b>\n",
+        numbers, d
+    ));
+
+    Ok(response)
 }
