a38d70d8887950fdf3d714c8f1fa6735bbd7d692
Egor Ternovoy <cofob@riseup.net>
2022-10-22 17:49:52 +0700

    Add docstings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use std::io::Write;
 use std::str::FromStr;
 
+/// Get common divisor of two numbers
+///
+/// # Examples
+/// Common divisor of 2 and 4 is 2
+/// ```
+/// assert_eq!(gcd(2, 4), 2);
+/// ```
+///
+/// # Panics
+/// Panics if any of the arguments is zero
+/// ```
+/// gcd(0, 1);
+/// ```
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
@@ -21,7 +34,9 @@ fn test_gcd() {
     assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
 }
 
+/// Main function
 fn main() {
+    // Check if arguments are given
     if std::env::args().len() == 1 {
         writeln!(
             std::io::stderr(),
@@ -32,15 +47,18 @@ fn main() {
         std::process::exit(1);
     }
 
+    // Check if all required arguments are given
     if std::env::args().len() != 3 {
         writeln!(std::io::stderr(), "Error: You must specify two numbers.").unwrap();
         std::process::exit(1);
     }
 
+    // Convert arguments to numbers
     let numbers: Vec<u64> = std::env::args()
         .skip(1)
         .map(|arg| u64::from_str(&arg).expect("error parsing argument"))
         .collect();
 
+    // Calculate gcd of two numbers and print it
     println!("Output: {}", gcd(numbers[0], numbers[1]));
 }
